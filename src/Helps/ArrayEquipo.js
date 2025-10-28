c2d0c5efd1a4a672d9801b0f8609493c017d1bac
fmarcos38 <fmarcos_23@hotmail.com>
2025-10-28 08:23:17 -0300

    detalles finales

--- src/Helps/ArrayEquipo.js
+++ src/Helps/ArrayEquipo.js
@@ -10,7 +10,7 @@ const ezequiel = {
 const ArrayEquipo = [
     
     {
-        imgPersona: ImgFran, nombre: 'Fran Izzo', texto: 'Ventas', 
+        imgPersona: ImgFran, nombre: 'Francisco Izzo', texto: 'Comercial', 
     },
     {
         imgPersona: ImgCintia, nombre: 'Cintia', texto: 'Administración', 
